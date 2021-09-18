7e871c6b10c897dbba3dd7fde09e0a9660851c39
DESKTOP-1SAUT79\S <noreply>
2021-09-18 19:31:07 +0900

    feat: 表示される文字を調整

--- MineSweeper/Display.cpp
+++ MineSweeper/Display.cpp
@@ -2,13 +2,22 @@
 #include "Display.hpp"
 
 void Display::Show(Stage aStage) {
+	const std::string fullNum[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10" };
+	std::cout << "  ";
+
+	for (int i = 0; i < aStage.getWidth(); i++) {
+		std::cout << fullNum[i];
+	}
+	std::cout << std::endl;
+
 	for (int i = 0; i < aStage.getHeight(); i++) {
+		std::cout << fullNum[i];
 		for (int j = 0; j < aStage.getWidth(); j++) {
 			if (aStage.isBomb(j, i)) {
-				std::cout << '*';
+				std::cout << "–";
 			}
 			else {
-				std::cout << ' ';
+				std::cout << "  ";
 			}
 		}
 		std::cout << std::endl;
